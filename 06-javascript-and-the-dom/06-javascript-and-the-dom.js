8c51b952bbc337db22a9804349c0e8bdd293bb57
Jonathan Spurling <jon@jonspurling.ca>
2024-09-04 22:26:44 -0500

    Added to the JavaScript&DOM Section

---
+++ 06-javascript-and-the-dom/06-javascript-and-the-dom.js
@@ -0,0 +1,95 @@
+const para = document.querySelector('p'); // Place a css selector in the querySelector method to select the first paragraph element in the HTML file
+
+console.log(para); // Log the first paragraph element to the console
+
+const secondPara = document.querySelector('.error'); // Place a css selector in the querySelector method to select the first paragraph element with a class of 'error' in the HTML file
+
+console.log(secondPara); // Log the first paragraph element with a class of 'error' to the console
+
+const divError = document.querySelector('div.error'); // Place a css selector in the querySelector method to select the first div element with a class of 'error' in the HTML file
+
+// Selecting multiple elements at once
+const paragraph = document.querySelectorAll('p'); // Place a css selector in the querySelectorAll method to select all paragraph elements in the HTML file
+
+// console.log(paragraph); // Log all paragraph elements to the console
+// console.log(paragraph[3]); // Log the fourth paragraph element to the console
+
+paragraph.forEach(para => {
+    // console.log(para); // Log each paragraph element to the console
+}); // You can use the forEach method to loop through each paragraph element and log them to the console
+
+const errors = document.querySelectorAll('.error'); // Place a css selector in the querySelectorAll method to select all paragraph elements with a class of 'error' in the HTML file
+
+// console.log(errors); // Log all elements with a class of 'error' to the console
+
+const title = document.getElementById('page-title');
+// console.log(title) // Log the element with an id of 'page-title' to the console
+
+const errors2 = document.getElementsByClassName('error'); // Place a class name in the getElementsByClassName method to select all elements with a class of 'error' in the HTML file
+
+// console.log(errors2); // Log all elements with a class of 'error' to the console
+
+const paras83 = document.getElementsByTagName('p'); // Place a tag name in the getElementsByTagName method to select all paragraph elements in the HTML file
+// console.log(paras83); // Log all paragraph elements to the console
+// console.log(paras83[2]); // Log the third paragraph element to the console
+
+// Changing text content
+const para1 = document.querySelector('p'); // Place a css selector in the querySelector method to select the first paragraph element in the HTML file
+
+// console.log(para1.innerText); // Log the text content of the first paragraph element to the console
+para1.innerText = 'Ninjas are awesome!'; // Change the text content of the first paragraph element to 'Ninjas are awesome!'
+// console.log(para1.innerText); // Ninja's Are Awesome!
+
+// If you wanted to append to the text you could use the += operator
+// para1.innerText += ' Ninjas are awesome!'; // Append the text content of the first paragraph element to ' Ninjas are awesome!'
+
+const paras = document.querySelectorAll('p'); // Place a css selector in the querySelectorAll method to select all paragraph elements in the HTML file (as a node list)
+
+paras.forEach(para => {
+    // console.log(para.innerText); // Log each paragraph element's text content to the console
+    paras.innerText += ' new text'; // Append ' new text' to each paragraph element's text content
+}); // You will see new text appended to each paragraph element's text content
+
+// Changing HTML content
+const content = document.querySelector('.content'); // Place a css selector in the querySelector method to select the first element with a class of 'content' in the HTML file
+
+// Thats how we get it, how do we update it?
+content.innerHTML = '<h2>This is a new h2</h2>'; // Change the HTML content of the first element with a class of 'content
+
+// If I wanted to just append to the HTML content I could use the += operator
+content.innerHTML += '<h2>This is a new new h2</h2>'; // Append the HTML content of the first element with a class of 'content
+
+// We want to create a little HTML template for each person in the array that we got from the "database".
+const people = ['mario', 'luigi', 'yoshi']; // Create an array of strings called people
+
+people.forEach(person => {
+    content.innerHTML += `<p>${person}</p>`; // Append a paragraph element with the string value of each person in the people array
+}); // Use the forEach method to loop through each person in the people array
+
+// Getting attributes
+const link = document.querySelector('a'); // Place a css selector in the querySelector method to select the first anchor element in the HTML file
+// console.log(link.getAttribute('href')); // Log the href attribute of the first anchor element to the console
+
+// Setting attributes
+link.setAttribute('href', 'https://www.jonspurling.ca'); // Set the href attribute of the first anchor element to 'https://www.jonspurling.ca'
+link.innerText = 'Jon Spurling'; // Set the text content of the first anchor element to 'Jon Spurling'
+
+
+const msg = document.querySelector('p'); // Place a css selector in the querySelector method to select the first paragraph element in the HTML file
+// console.log(msg.getAttribute('class')); // Log the class attribute of the first paragraph element to the console
+
+ msg.setAttribute('class', 'success'); // Set the class attribute of the first paragraph element to 'success'
+ 
+ // Changing CSS styles
+ msg.setAttribute('style', 'color: green;'); // Set the style attribute of the first paragraph element to 'color: green;'
+
+// Get the color of the h1 element
+const title2 = document.querySelector('h1'); // Place a css selector in the querySelector method to select the first h1 element in the HTML file
+
+title2.style.margin = '50px'; // Set the margin style of the first h1 element to '50px'
+
+// Say we want to change the color style
+title2.style.color = 'crimson'; // Set the color style of the first h1 element to 'crimson'
+
+title2.style.fontSize = '60px'; // Set the font size style of the first h1 element to '60px'
+
--- 06-javascript-and-the-dom/06-javascript-and-the-dom.md
+++ 06-javascript-and-the-dom/06-javascript-and-the-dom.md
@@ -1,5 +1,71 @@
 ## JavaScript & the DOM
 
+In this chapter, you really get to see what JavaScript is about and start manipulating the DOM. In this chapter you will learn to:
+
+- add content to the browser
+- change CSS styles
+- react to user events (e.g clicking)
+- cool effects like popups
+
+### DOCUMENT OBJECT MODEL (DOM)
+**What is it?**
+It's created by the browser, and it's called the ***'document'*** object. So if we want to change something on the web page, we just interact with the ***'document'*** object.
+
+In a browser, if you go into the console in developer tools, and type document, you are going to see a visual representation of the html page. We can expand them and go inside them. There's a ton of properties assosiated with the ***'document'*** object, and if you use dot notation (add a . to the end of ***document.***) it will show you all of them.
+
+The document describes are html page as a hierarcial tree of nodes. If you think of an html page it has a whole bunch of tags. At the beginning we have an html page (root node). Then we have a head and body node. Under those we could branch off to title node and h1, div to p, or just div.
+
+![Image depicting how the DOM sees the hierarcial tree of our HTML page](../assets/hierarcial-tree.png)
+
+### ADD/CHANGE/REMOVE CONTENT
+The first step to add, change or remove content, you need to select what content you want to alter. Then you need to reach in, and get a reference on the element. This is known as querying the dom.
+
+The second step is when we actually do something, like change its content. We can do this using a few methods attached to document such as:
+
+- querySelector('css-selector'); -> This will select the first element that matches the targeted css selector.
+- querySelectorAll('css-selector'); -> This one will get all the tags that you have targeted with the css selector.
+**NOTE**: These are the recommended ways to grab an element. The other ways are possible if needed.
+
+### Other Ways
+These other ways are older ways of doing it, but still may be useful at times. 
+
+**getElementById('page-title')**
+Pulls a single value from the element with an ID of page-title
+
+**getElementsByClassName('error')**
+Pulls multiple values from the document that have a class name of *"error"*.
+
+**getElementsByTagName('p')**
+Pulls all the p tags from the document.
+
+All these must should be stored in a variable.
+
+### MAKE A COOL POP-UP EFFECT IN THE WEB PAGE
+**How to change the text & html inside elements**
+To change the text inside an element, you would grab the element using a querySelector or other method, and use .innerText to change or append.
+
+For example:
+    error.innerText = "this is the error message"; // This will change the error message entirely of a single element.
+    error.innerText += "this is additional text"; // This will append to the original error elements inner text.
+
+**Changing the HTML**
+To change the HTML inside an element you would grab the element using another querySelector or other method, and use .innerHTML to change or append new HTML.
+
+For example:
+    error.innerHTML = "<p>BE WARE</p>"; // New HTML
+    error.innerHTML += "<p>Oh, and this would be appended.</p>"; // This would get appended because we used the += operator.
+
+**Changing element attributes**
+We need to get a reference to the element so we would use a querySelector and store it in a variable.
+We can use the .getAttribute('href') to target the a tag in the index.html.
+To change an attribute, you can use the .setAttribute('what you want to change', 'the information your changing to');
+Using .setAttribute, you can also set styles by using 'style' for what you want to change, and then giving the 'color: green' or other css style.
+
+There is one drawback to using .setAttribute to the style. If we want to add another style to it, it will override instead.
+To prevent this in JavaScript by using the ***style*** property. (console.log(title.style))
+
+If you are ever unsure about a style property go to console in developer tools and look through the list.
+
 See [Examples](06-javascript-and-the-dom.js)
 
 
